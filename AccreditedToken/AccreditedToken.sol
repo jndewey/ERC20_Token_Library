a2940f950492e008a3a200d5cbcdb7855e220178
jndewey <jndewey@gmail.com>
2017-08-14 03:02:43 -0400

    update

--- AccreditedToken/AccreditedToken.sol
+++ AccreditedToken/AccreditedToken.sol
@@ -3,29 +3,38 @@ pragma solidity ^0.4.11;
 import './ERC20.sol';
 
 
+contract AccreditationRegistry {
+    function get(string _name) constant returns (address);
+    function getOrThrow(string _name) constant returns (address);
+}
+
+
+contract Accreditation {
+    function isAccredited(address _address) constant returns (bool);
+}
+
+
 contract AccreditedToken is ERC20 {
 
-    //Mapping of all approved investors who have been verified TODO: ADD VALUE FOR TIME ADDED
-    mapping(address => bool) public approvedInvestors;
-
-    /**
-     * @dev Checks modifier and allows transfer if accreditation of proposed transferee address has been verified.
-     * @param _to The address that will receive the tokens.
-     * @param _value The amount of tokens to be transferred.
-     */
-    modifier accreditationVerified (address investorAddress) {
-        //Check tokenRegistered value to determine if registration statement filed
-        //If registration filed, modifier evaluates to true and returns to function
-        //If false, then proceeds to verify investor
-        //Evaluate if investorAddress stored in map of approved accredited investors
-        //Require bool value returned to evaluate to true
-        //otherwise, exit function
-        _;
+    AccreditationRegistry accreditationRegistry = AccreditationRegistry(0xBbfc11cD1eC8591C96CC0985108ac82a73e7dCf4);
+    
+    function getEntitlement() constant returns(address) {
+        return accreditationRegistry.getOrThrow("com.hk.token");
     }
 
     /**
+    * @dev Checks modifier and allows transfer if accreditation of proposed transferee address has been verified.
+    * Require bool value returned to evaluate to true.
+    * Otherwise, exit function.
+    */
+    modifier accreditationVerified {
+        require(Accreditation(getEntitlement()).isAccredited(msg.sender));
+        _;
+    }
+    
+    /**
     * @dev Checks whether it can transfer or otherwise throws. 
-    * NOTE THIS RELATES TO TRANSFERFROM FUNCTION--NOT ACCREDITATION STATUS
+    * NOTE THIS RELATES TO TRANSFER FROM FUNCTION--NOT ACCREDITATION STATUS
     */
     modifier canTransfer(address _sender, uint256 _value) {
         require(_value <= transferableTokens(_sender, uint64(now)));
@@ -47,7 +56,7 @@ contract AccreditedToken is ERC20 {
      * @param _to The address that will receive the tokens.
      * @param _value The amount of tokens to be transferred.
      */
-    function establishAccreditation() returns (bool) {
+    f//unction establishAccreditation() returns (bool) {
         //address _investor = msg.sender;
         //Call API
         //if true value returned, then add _investor address to map of approved investors;
